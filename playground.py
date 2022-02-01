f7e14f04a1a3dc7eb3d9bd7a5b61e623267e0e07
a name <69024184+gosqib@users.noreply.github.com>
2022-02-01 14:29:50 -0500

    Update and rename huh.py to playground.py

--- playground.py
+++ playground.py
@@ -1,7 +1,7 @@
 from typing import Annotated, Any, Generator, List, Literal, Tuple, Union
 import cv2 as cv # type: ignore
 import numpy as np
-from bruh import *
+from infofuncs import *
 
 ELEPHANT: np.ndarray = cv.imread('random.jpg')
 TOLLFACE: np.ndarray = cv.imread('220px-Trollface_non-free.png')
