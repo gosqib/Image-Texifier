1cd4f3825d34b490eb1d96701543f884604498e4
a name <69024184+gosqib@users.noreply.github.com>
2022-02-03 11:26:35 -0500

    Update playground.py

--- playground.py
+++ playground.py
@@ -6,16 +6,6 @@ from infofuncs import *
 ELEPHANT: np.ndarray = cv.imread('random.jpg')
 TOLLFACE: np.ndarray = cv.imread('220px-Trollface_non-free.png')
 
-
-
-# cv.imshow('a', ELEPHANT)
-# cv.imshow('t', THRESH)
-# cv.imshow('a', texify(THRESH))
-
-# print(ELEPHANT.shape)
-# print(PROPORTIONED.shape)
-
-# print(tuple(texify(ELEPHANT, text_width_min=800, spec_height=22, spec_width=8)))
 # print(
 #     ''.join(tuple(texify(ELEPHANT, text_width_min=800, spec_height=22, spec_width=8)))
 # )
